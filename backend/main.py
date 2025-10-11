6c6a5fc6b8d75b5e57527c77cb07c59644b880d0
Danilo Paes <danilo.paes@pm.me>
2025-10-11 21:30:15 +0200

    fix: unused import on main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, Request, status, Depends
 from api.core.dependencies.authjwt import JWTValidation
+from fastapi import Depends
+from fastapi import FastAPI
+from fastapi import Request
 
 
 app = FastAPI()
